d124f39983a638ed38c1a2312fc46e2a589d38f9
Guilherme C. Rikaczewski <guilhermecrikaczewski@gmail.com>
2026-08-25 15:25:50 -0300

    feat: adiciona logotipo no header

--- src/shared/components/AppHeader/AppHeader.jsx
+++ src/shared/components/AppHeader/AppHeader.jsx
@@ -1,5 +1,9 @@
 import './AppHeader.css';
 
 export function AppHeader() {
-  return <header className="app-header" aria-label="Cabeçalho principal" />;
+  return (
+    <header className="app-header" aria-label="Cabeçalho principal">
+      <img src="/logo.png" alt="" className="logo-header"/>
+    </header>
+  );
 }
